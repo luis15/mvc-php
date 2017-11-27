1a5c3f7cd2eba6ad1f32173b6379c329c1a45eec
luis15 <coelho.luisantonio@hotmail.com>
2017-11-27 21:07:11 -0200

    curxo MVC Concluido

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
-require "controller/controller.php";
+require "controller/homeController.php";
 
-$ctrl = new Controller();
+$pagina = isset($_GET['pagina'])?$_GET['pagina']:"index";
 
-$ctrl->index();
+$ctrl = new homeController();
+
+switch($pagina){
+  case "index":
+  $ctrl->index();
+  break;
+  case "login":
+  $ctrl->login();
+  break;
+  case "painel":
+  $ctrl->painel();
+  break;
+}
